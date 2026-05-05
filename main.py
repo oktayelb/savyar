b367f8b225df9be914361645a6310848b97c4909
oktayelb <oktayelb@gmail.com>
2026-05-05 18:11:20 +0300

    old import change

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from app_new.cli import AppCLI
+from app.cli import AppCLI
 
 
 ## TODO  arapça sözsonu ünsüz ikilemesi
 ## his hissim zan zannetmek  vs
-
+##TODO improved beam search?
 ##TODO ler ve le r ayrışımını nasıl öğreteceğiz?
 
 ## TODO maksızın anlaşılmıyor, ın geri mi gelsin yoksa maksızın mı olsun
